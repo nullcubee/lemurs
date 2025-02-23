217b2012c6e0398d18ae889773a94e72781e20cc
NullCube <nullcub3@gmail.com>
2025-02-23 15:48:38 -0800

    flake: use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,66 +1,90 @@
 {
-  description = "A flake to build a Rust project to WASM";
+  description = "Lemurs: A customizable TUI display/login manager written in Rust";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    utils.url = "github:numtide/flake-utils";
-
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
-      self,
       nixpkgs,
-      utils,
       rust-overlay,
-    }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        packageName = "lemurs";
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ flake-parts.flakeModules.easyOverlay ];
 
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default ];
+      # System types to support.
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          ...
+        }:
+        let
+          pname = "lemurs";
+          version = "0.4.0-nightly";
+          rust-toolchain = pkgs.rust-bin.stable.latest.default;
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rust-toolchain;
+            rustc = rust-toolchain;
+          };
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
+
+          overlayAttrs = config.packages;
+
+          packages.default = rustPlatform.buildRustPackage {
+            inherit pname version;
+            src = ./.;
+
+            postPatch = ''
+              substituteInPlace extra/config.toml \
+                --replace-fail "/usr/sh" "${pkgs.bash}/bin/bash"
+
+              substituteInPlace extra/config.toml \
+                --replace-fail "/usr/bin/X" "${pkgs.xorg.xorgserver}/bin/X"
+
+              substituteInPlace extra/config.toml \
+                --replace-fail "/usr/bin/xauth" "${pkgs.xorg.xauth}/bin/xauth"
+            '';
+
+            buildInputs = [
+              pkgs.linux-pam
+            ];
+
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              linux-pam
+              rust-toolchain
+              cargo-dist
+            ];
+          };
         };
-
-        rustToolchain = pkgs.rust-bin.stable.latest.default;
-
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
-        };
-      in
-      {
-        packages.default = rustPlatform.buildRustPackage {
-          name = packageName;
-          src = ./.;
-
-          postPatch = ''
-            substituteInPlace extra/config.toml \
-              --replace-fail "/usr/sh" "${pkgs.bash}/bin/bash"
-
-            substituteInPlace extra/config.toml \
-              --replace-fail "/usr/bin/X" "${pkgs.xorg.xorgserver}/bin/X"
-
-            substituteInPlace extra/config.toml \
-              --replace-fail "/usr/bin/xauth" "${pkgs.xorg.xauth}/bin/xauth"
-          '';
-
-          buildInputs = [
-            pkgs.linux-pam
-          ];
-
-          cargoLock.lockFile = ./Cargo.lock;
-        };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            linux-pam
-            rustToolchain
-            cargo-dist
-          ];
-        };
-      }
-    );
+    };
 }
